b3230967ba37fe9acc695c9169f99ca054b5d549
Christoph Heinen <heinen@fachschaft.info>
2019-11-07 17:51:00 +0100

    wahlbekanntmachung kann -Fachschaft einfügen -Datum einfügen -Zahl der zu wählenden Mitglieder berechnen

--- js/wahlfristen.js
+++ js/wahlfristen.js
@@ -11,9 +11,25 @@ function bekanntmachung_onload(){
     var url = new URL(url_string);
     var first_day = url.searchParams.get("start");
     var last_day = url.searchParams.get("end");
-    $( "#datestart" ).datepicker( "setDate",new Date(first_day) )
-    $( "#dateend" ).datepicker( "setDate",new Date(last_day) )
+    var first_day = $( "#datestart" ).datepicker( "getDate" );
+    first_day = new Date(first_day)
+    
+    var last_day  = $( "#dateend" ).datepicker( "getDate" );
+    last_day = new Date(last_day)
+    
     updateDates(first_day, last_day)
+}
+
+function update_button_clicked(){
+    update_fsvfsr()
+    update_fs_name()
+    
+    var first_day = $( "#datestart" ).datepicker( "getDate" );
+    var last_day  = $( "#dateend" ).datepicker( "getDate" );
+    
+    var formatstring = "dd.mm.yy";
+    update_wahlzeitraum($.datepicker.formatDate( formatstring, first_day),$.datepicker.formatDate( formatstring, last_day))
+    
 }
 
 function update(){
@@ -25,15 +41,16 @@ function update(){
   }
 }
 
+// fügt parameter in die URL für den Aufruf zur Wahlbekanntmachung ein
 function updateBekanntmachungLink(first_day, last_day){
     var formatstring = "yy-mm-dd"
     document.getElementById("wahlbekanntmachung_link").href = wahlbekanntmachung_link + "start=" + $.datepicker.formatDate( formatstring, first_day ) + "&end=" + $.datepicker.formatDate( formatstring, last_day )
 }
 
-
-
-function updateDates(first_day, last_day){
-  var wochentag = ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag' ]
+// updatet den Wochentag hinter den Eingabefeldern für erster und letzter Wahltag
+// prüft außerdem ob die Tage legitim sind
+function updateWochentag(first_day, last_day){
+    var wochentag = ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag' ]
   d_first = new Date(first_day)
   $('#wochentag_1').text( wochentag[d_first.getDay()])
   if (d_first.getDay() > 0 && d_first.getDay() < 4)
@@ -47,6 +64,61 @@ function updateDates(first_day, last_day){
     $("#wochentag_2").css("color","green");
   else 
     $("#wochentag_2").css("color","red");
+}
+
+//updatet das zu wählende Gremium
+function update_fsvfsr(gremium)
+{   
+    var gremium = "Fachschaftsrat"
+    var gremium_artikel = "den"
+    var zahl_sitze = 11
+    if ($( "#anzwahlberechtigte" ).val() > 500)
+    {
+        gremium = "Fachschaftsvertretung"
+        gremium_artikel = "die"
+        if($( "#anzwahlberechtigte" ).val() > 2000)
+            zahl_sitze = 19
+        else if($( "#anzwahlberechtigte" ).val() > 2000)
+            zahl_sitze = 15
+    }
+    if ($( "#fsvsatzung" ).is(":checked"))
+    
+    {
+        gremium = "Fachschaftsvertretung"
+        gremium_artikel = "die"
+    }
+    for (const element of document.getElementsByClassName('fsvfsr')){
+        element.innerHTML = gremium
+    }
+    for (const element of document.getElementsByClassName('derdesfsvfsr')){
+        element.innerHTML = gremium_artikel
+    }
+    for (const element of document.getElementsByClassName('zahl_sitze')){
+        element.innerHTML = zahl_sitze
+    }
+}
+
+// updatet den Name der Fachschaft
+function update_fs_name()
+{
+    for (const element of document.getElementsByClassName('fachschaft')){
+        element.innerHTML = $( "#fsname" ).val()
+    }
+}
+
+//updatet den Wahlzeitraum
+function update_wahlzeitraum(first_day,last_day)
+{
+    var zeitraum = "von "+ first_day + " bis " + last_day
+   
+    for (const element of document.getElementsByClassName('wahlzeitraum')){
+        element.innerHTML = zeitraum
+    }
+}
+
+function updateDates(first_day, last_day){
+  
+  updateWochentag(first_day, last_day)
  
   var formatstring = "dd.mm.yy";
   //Festlegung Wahltermin 
